08e279d5426ab84e5232f4472a94503d9ade1f58
SergeyGoo <blinovsr@gmail.com>
2019-05-26 12:55:39 +0300

    Изменение 7 домашней работы

--- homework_7.1.c
+++ homework_7.1.c
@@ -9,18 +9,17 @@ int entering(){
 	scanf("%s", &name);
 	printf("\nEnter number:");
 	scanf("%s", &number);
-	fprintf(pbook,"%s\n%s\n", name, number);
+	fprintf(pbook,"%s %s\n", name, number);
 	fclose (pbook);
 }
 
 int output(){
 	char *str[100];
-    char *estr;
 	FILE *phbook = fopen("PhoneBook.txt", "r");
 
     while (1)
     {
-         estr = fgets (str,sizeof(str),phbook);
+         fgets (str,sizeof(str),phbook);
          if ( feof (phbook) != 0)
          {  
             break;
@@ -34,7 +33,7 @@ int output(){
 
 int search(){
     FILE *phnbook = fopen("PhoneBook.txt", "r+");
-    char *p;
+    char p;
     p = strstr(phnbook, "Ser");
     printf("%s", p);
     return 0;
@@ -47,13 +46,24 @@ int main(){
 	while (i!=0){
 		printf("Now you are in main menu, type \n\"1\" for entering numbers, \n\"2\" for printing all numbers on screen, \n\"3\" for searching numbers, \n\"0\" for exit\n");
 		scanf("%d", &i);
-		if (i==1) {
-		printf("Your choose is entering of numbers, \nenter names and numbers \ntype \"0\" for exit");
-		entering();}
-		else if (i==2) output();
-		else if (i==3) search();
-		else if (i!=0) printf ("Try another button\n");
-		
-}
-printf ("Goodbye!\n");
+		switch (i) {
+			case 1:
+				printf("Your choose is entering of numbers, \nenter names and numbers \ntype \"0\" for exit");
+			entering();
+				break;
+			case 2:
+			output();
+				break;
+			case 3:
+			search();
+				break;
+			case 0:
+			printf ("Goodbye!\n");
+				break;
+			default: 
+				printf ("Try another button\n");
+				break;
+		}
+	}
+
 }
